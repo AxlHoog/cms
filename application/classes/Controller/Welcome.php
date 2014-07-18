113d6f10d8f5ce7b9a61ee1ab3b4f52a8d9d23b6
Deathreapert <axl.hoogelander@gmail.com>
2014-07-19 00:36:31 +0200

    Cleaning
    Cleaned the Welcome Controller and Welcome View, for the readability,

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,12 +4,7 @@ class Controller_Welcome extends Controller_Main_Frontend {
 
 	public function action_index()
 	{
-		
-
-		
-			$this->template->content = View::factory('frontend/page/welcome');
-			
-		
+		$this->template->content = View::factory('frontend/page/welcome');		
 	}
 
 } // End Welcome
